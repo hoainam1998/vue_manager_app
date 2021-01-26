a45de1b713b40f0d3e7bdb0d2ecb8ed9ce0995f0
HoaiNam <hoaimongktk20@gmail.com>
2021-01-27 05:52:32 +0700

    reuse table

---
+++ src/components/Custom_table.vue
@@ -0,0 +1,16 @@
+<template>
+  <Table
+    :items="items"
+    :fields="fields"
+    :striped="true"
+  ></Table>
+</template>
+
+<script>
+import Table from './Table';
+export default {
+  components: {Table},
+
+  props: ["fields", "items"],
+};
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,23 @@
+
+<script>
+export default {
+    name: "Table",
+    props: [
+          'items',
+          'fields'
+        ],
+  render(h) {
+    return h('b-table', {
+       props: Object.assign(this.$attrs, this.$props),
+      slots: this.$parent.$slots,
+      scopedSlots: this.$parent.$scopedSlots,
+      on: {
+        //'row-clicked': (item, index, event) => alert('clicked ' + index)
+      }
+    });
+  }
+}
+</script>
+<style scoped>
+
+</style>
